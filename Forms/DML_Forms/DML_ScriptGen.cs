8090c7f9b47970848bbf6f3a096a17d0ad00ace8
mdhole <manthan.dhole@infor.com>
2023-08-16 12:17:03 +0530

    New forms added for DDL, DML, DQL

---
+++ Forms/DML_Forms/DML_ScriptGen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQL_ScriptGenUtility.Forms.DML_Forms
+{
+    public partial class DML_ScriptGen : Form
+    {
+        public DML_ScriptGen()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Forms/DQL_Forms/DQL_ScriptGen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQL_ScriptGenUtility.Forms.DQL_Forms
+{
+    public partial class DQL_ScriptGen : Form
+    {
+        public DQL_ScriptGen()
+        {
+            InitializeComponent();
+        }
+    }
+}
